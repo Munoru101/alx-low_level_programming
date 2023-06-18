29f561f860836623463118ec829410fb6f8f0589
Munoru 101 <dennismunoru@gmail.com>
2023-06-18 01:34:19 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** void tells C there are no arguments
+ * printf prints out to standard output what is in parentheses
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
