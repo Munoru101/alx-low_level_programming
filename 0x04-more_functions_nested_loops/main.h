ab678481feb0716d79a4fe43f2785ec52bbccf30
Munoru 101 <dennismunoru@gmail.com>
2023-06-23 04:24:04 -0700

    dig deeper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#include <ctype.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+
+int main(void);
+int _isupper(int c);
+{
+	int c;
+	_isupper = isupper(int c);
+	if(_isuppper == 0)
+	{
+		printf("A: %d\n", 1);
+	}
+	else
+	{
+		printf("a: %d\n", 0);
+	}
+	return (0);
+}
+
