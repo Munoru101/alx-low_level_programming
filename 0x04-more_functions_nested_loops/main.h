86ef98a912bbaaa580839bb748716f0bb1a2cc13
Munoru 101 <dennismunoru@gmail.com>
2023-06-23 04:48:05 -0700

    corrected syntax of main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,23 +1,6 @@
-#include <ctype.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - entry
- * Return: 0
- */
-
-int main(void);
 int _isupper(int c);
-{
-	int c;
-	_isupper = isupper(int c);
-	if(_isuppper == 0)
-	{
-		printf("A: %d\n", 1);
-	}
-	else
-	{
-		printf("a: %d\n", 0);
-	}
-	return (0);
-}
 
+#endif
