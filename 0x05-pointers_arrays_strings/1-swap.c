ce4e412338c41d91d5863097328d4d3ead7b386b
Munoru 101 <dennismunoru@gmail.com>
2023-06-26 08:59:51 -0700

    Dont swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,11 +2,14 @@
 
 /**
  * swap_int - a function to swap two integers
+ * @a: pointer to an integer
+ * @b: pointer to an integer
  */
 
 void swap_int(int *a, int *b)
 {
 	int tmp;
+
 	tmp = *a;
 	*a = *b;
 	*b = tmp;	
