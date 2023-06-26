419c5057a35d553e4e3449b03b9a867423416a7e
Munoru 101 <dennismunoru@gmail.com>
2023-06-26 09:35:46 -0700

    This report by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - a function that returns length of a string
+ * @s: character array
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	return strlen(s);
+}
