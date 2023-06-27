cb2fd23dc1e33a799a6c9c22344103b9e327854b
Munoru 101 <dennismunoru@gmail.com>
2023-06-27 04:06:39 -0700

    Half the lies arent true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,10 +14,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0Holberton')
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
