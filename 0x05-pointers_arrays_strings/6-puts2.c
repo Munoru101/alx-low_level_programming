d67db439809ad90e2dabedca7d4b6479fa2f39d9
Munoru 101 <dennismunoru@gmail.com>
2023-06-27 03:57:03 -0700

    Half the lies arent true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * puts2 is a function that prints every other
+ * puts2 - is a function that prints every other
  * character of a string
  * @str: string
  * starting with the first character, followed by
@@ -13,7 +13,8 @@
 void puts2(char *str)
 {
 	int i = 0;
-	while(str[i] != '\0')
+
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i += 2;
