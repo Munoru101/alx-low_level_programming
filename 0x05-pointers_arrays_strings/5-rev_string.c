e713adf521ca7b086c9ae50ee3bba44ae9f76f64
Munoru 101 <dennismunoru@gmail.com>
2023-06-27 02:41:16 -0700

    A good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: pointer to the string to be reversed
+ * @n: length of string
+ * Return: nothing.
+ */
+
+void rev_string(char *s)
+{
+	int n = strlen(s);
+	int i, j;
+
+	for(i = 0, j = n -1; i < j; i++, j--)
+	{
+		swap_int(&i, &j);
+	}
+}	
