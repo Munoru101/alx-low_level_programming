c3c61c56f385d31bf04477bc324d0267a7e27c81
Munoru 101 <dennismunoru@gmail.com>
2023-06-30 01:51:00 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strcmp is a function that compare string s1 to s2
+ * @s1: string 1
+ * @s2: string 2
+ * Return: an integer representing the comparison result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return strcmp(s1, s2);
+}
