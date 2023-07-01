46d600f6f6e7d66b7a78cf56b58d69d9c675845f
Munoru 101 <dennismunoru@gmail.com>
2023-07-01 13:06:51 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry function into the program
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char alphabet;
+	char _Alphabet;
+
+        alphabet = 'a';
+        while (alphabet <= 'z')
+        {
+                putchar(alphabet);
+                alphabet++;
+        }
+	_Alphabet = 'A';
+	while (_Alphabet <= 'Z')
+	{
+		putchar(_Alphabet);
+		_Alphabet++;
+	}
+        putchar('\n');
+        return (0);
+}
