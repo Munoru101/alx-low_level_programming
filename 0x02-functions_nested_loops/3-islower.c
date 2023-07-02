f0e3194b315896e439348e3beb07384e73bdfca1
Munoru 101 <dennismunoru@gmail.com>
2023-07-02 11:47:06 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _islower- function taht checks for lowercase character
+ * Return: 1 if character is lowercase and 0 otherwise
+ * @c: variable representing the character
+ */
+int _islower(int c)
+{
+	return islower(c);
+}
