38a96977528fa323342a22d199c8c3c3420fbff0
Munoru 101 <dennismunoru@gmail.com>
2023-07-02 00:50:51 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,19 +15,18 @@ int main(void)
 
 	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = num1+1; num2 <= 99; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar('0' + (num1 / 10));
 			putchar('0' + (num1 % 10));
 			putchar(' ');
 			putchar('0' + (num2 / 10));
 			putchar('0' + (num2 % 10));
-			if (num1 != 99 || num2 != 99)
+			if (num1 != 98 || num2 != 99)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-			
 		}
 	}
 	putchar('\n');
