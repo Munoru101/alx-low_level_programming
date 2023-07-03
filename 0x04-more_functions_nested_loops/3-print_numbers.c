8320cc2f9cb2e1d7fc683a884c070ec864e74e99
Munoru 101 <dennismunoru@gmail.com>
2023-07-03 02:11:06 -0700

    memset

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * function that prints the numbders from 0 to 9
+ * followed by a new line
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+		_putchar('\n');
+	}
+}
