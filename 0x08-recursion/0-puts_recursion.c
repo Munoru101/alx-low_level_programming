63b209a32ea11f76e32d11156b45f7af0a10ed18
Munoru 101 <dennismunoru@gmail.com>
2023-07-05 08:00:30 -0700

    She locked away a secret, dep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string followed by a new line
+ * @s: character pointer
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	_puts_recursion(s++);
+}
