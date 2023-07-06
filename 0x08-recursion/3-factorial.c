ea6107ba84941e80edfd2d5b2f6da8bc6522f2c0
Munoru 101 <dennismunoru@gmail.com>
2023-07-06 05:29:31 -0700

    You must not be afraid to dram a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: integer argument
+ * Return: Always 0.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
