f064d205daf892526deee520372be2cf9fb5a526
Munoru 101 <dennismunoru@gmail.com>
2023-07-17 09:22:13 -0700

    Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef OBJECT_LIKE_MACRO
+#define OBJECT_LIKE_MACRO
+
+#define SIZE 1024
+/*This header file defines a macro named SIZE as an abbreviation for the token 1024*/
+int main(void);
+#endif /*OBJECT_LIKE_MACRO*/
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+C - Preprocessor. Use of macros and how to include guard your header files
