569464c9755a519b15a10c5678ae70846b990c2e
Munoru 101 <dennismunoru@gmail.com>
2023-07-17 09:39:03 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry to the program
+ * Program prints the name of the file
+ * It was compiled from
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Program compiled from the file: %s\n", __FILE__);
+	return (0);
+}
