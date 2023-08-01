3d7db5b01cf352805e42da5d3d5c6955b7c888d0
Munoru101 <dennismunoru@gmail.com>
2023-08-01 14:19:43 -0700

    Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,6 +13,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
