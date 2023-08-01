71a40195d0ca10405475120b3ab8bcae5258b0d1
Munoru101 <dennismunoru@gmail.com>
2023-08-01 13:39:56 -0700

    Removed unnecessary vars

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t node_num = 0;
-	const listint_t *current = h;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		printf("%d", current->n);
-		current = current->next;
-		if (current != NULL)
+		printf("%d", h->n);
+		h = h->next;
+		if (h != NULL)
 		{
 			printf(", ");
 		}
