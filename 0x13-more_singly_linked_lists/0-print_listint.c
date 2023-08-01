04015449459bc7d682d336cdf140847c7a7d1d66
Munoru101 <dennismunoru@gmail.com>
2023-08-01 13:09:27 -0700

    Logic to handle -ve numbers and prints commas between elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
+#include "lists.h"
 /**
  * print_listint - function that prints all the elements of
  * a listint_t list
  * @h: pointer to argument of type listint_t
  * Return: number of nodes
  */
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-}listint_t;
 size_t print_listint(const listint_t *h)
 {
 	size_t node_num = 0;
 	const listint_t *current = h;
+	int tmp, num, digits;
+
 	while (current != NULL)
 	{
-		printf("%d", current->n);
+		num = current->n;
+		digits = 1;
+		if (num < 0)
+		{
+			_putchar('-');
+			num = -num;
+		}
+		tmp = num;
+		while (tmp / 10 > 0)
+		{
+			digits *= 10;
+			tmp /= 10;
+		}
+		while (digits > 0)
+		{
+			_putchar('0' + num / digits);
+			num %= digits;
+		}
 		current = current->next;
 		if (current != NULL)
 		{
-			printf(",");
+			_putchar(',');
 			node_num++;
 		}
 	}
-	printf("\n");
+	_putchar('\n');
 	return (node_num);
 }
