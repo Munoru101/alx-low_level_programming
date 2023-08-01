e803ca800460392a9337088a4cdc497f36c7f526
Munoru101 <dennismunoru@gmail.com>
2023-08-01 11:55:47 -0700

    print elements of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * print_listint - function that prints all the elements of
+ * a listint_t list
+ * @h: pointer to argument of type listint_t
+ * Return: number of nodes
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+}	listint_t;
+size_t print_listint(const listint_t *h)
+{
+	size_t node_num = 0;
+	const listint_t *current = h;
+	while (current != NULL)
+	{
+		printf("%d", current -> n);
+		current = current -> next;
+		if (current != NULL)
+		{
+			printf(",");
+			node_num++;
+		}
+	}
+	printf("\n");
+	return (node_num);
+}
