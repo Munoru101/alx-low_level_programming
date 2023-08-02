1f0f4cc1f80ab0dd84455d0a28236e45f09c2473
Munoru101 <dennismunoru@gmail.com>
2023-08-02 04:53:39 -0700

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -27,8 +27,8 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return (NULL);
 	}
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
