88dec9077fb140462983689ab10a0e06171616d0
Munoru101 <dennismunoru@gmail.com>
2023-09-28 07:39:22 -0700

    Recursive functions that find and return a square root of a given number n

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+int _sqrt(int n, int x);
+int _sqrt_recursion(int n);
+/**
+ * _sqrt - finds natural square root of number n
+ * @n: the number to be squarerooted
+ * @x: the root of n
+ * Return: x if n has natural square root
+ * -1 if n has no natural square root
+ */
+int _sqrt(int n, int x)
+{
+	if ((x * x) == n)
+		return (x);
+	if ((x * x) > n)
+		return (-1);
+	return (_sqrt(n, x + 1));
+}
+/**
+ * _sqrt_recursion - returns the natural root of a number
+ * @n: the number to determine its square root
+ * Return: the root if n has a natural square root
+ * -1 if n has no natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	int x = 0;
+	
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+	return (_sqrt(n, x));
+}
