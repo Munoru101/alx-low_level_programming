3fc86a4225b8db4d9d9ecd486cab216fd44f2df1
Munoru101 <dennismunoru@gmail.com>
2023-09-28 12:12:32 -0700

    Recursive function to compare two strings if identical

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings
+ * @s1: the first string
+ * @s2: second string
+ * Return: 1 is strings are identical
+ * 0 if otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	if (*s1 == *s2 || *s2 == '*')
+		return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	return (0);
+}
