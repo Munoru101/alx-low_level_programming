be79c94d0171987f8f8441dba36e9c18e811a801
Munoru101 <dennismunoru@gmail.com>
2023-10-03 12:25:32 -0700

    Concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,9 @@ char *str_concat(char *s1, char *s2)
 	char *s1s2;
 	int i, s1Len = 0, s2Len = 0, j = 0;
 	
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+		return ("");
+	if (s2 == NULL)
 		return ("");
 	for (i = 0; s1[i] != '\0'; i++)
 	{
