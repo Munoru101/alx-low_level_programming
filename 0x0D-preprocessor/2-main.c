ec801f2894a485af063ed5636969ea7241076f60
Munoru101 <dennismunoru@gmail.com>
2023-10-09 13:23:40 -0700

    Standard predefined Macro, __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Description - function that prints the name of
+ * the file it was compiled from
+ * Main - entry to the progr
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
