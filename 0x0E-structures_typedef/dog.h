5c33266c7070a4c566861e1b8d91385467c34d4d
Munoru101 <dennismunoru@gmail.com>
2023-10-10 11:34:31 -0700

    My C struct called dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - a created struct called dog
+ * @name: of the dog
+ * @age: of the dog
+ * @owner: of the dog
+ * Description - dog has the elements/variables
+ * name, age, owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
