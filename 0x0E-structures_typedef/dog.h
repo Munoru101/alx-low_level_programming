885a642db2eb19df8a17a5537bf2c0da778b96d9
Munoru101 <dennismunoru@gmail.com>
2023-10-10 12:44:39 -0700

    Updated header file with typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,15 +13,9 @@ struct dog {
 	float age;
 	char *owner;
 };
-/**
- * typedef my_dog for struct dog
- */
-typedef struct dog my_dog;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/**
- * dog_t *new_dog(char *name, float age, char *owner);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-*/
 #endif
