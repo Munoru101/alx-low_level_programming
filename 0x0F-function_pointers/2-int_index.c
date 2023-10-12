be73091887859155ee4d52254603a1aa1c58c45c
Munoru101 <dennismunoru@gmail.com>
2023-10-12 09:10:18 -0700

    Parentheses and brackets are there for a reason

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int index;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
+	{
 		return (-1);
-	for (index = 0; index < size; index++)
+	}
+		for (index = 0; index < size; index++)
 	{
 		if (cmp(array[index] != 0))
+		{
 				return (index);
+		}
 	}
 	return (-1);
 }
