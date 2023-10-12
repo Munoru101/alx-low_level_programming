8e3d3a0d8f411506bb6095aee111cdc94d71e7a9
Munoru101 <dennismunoru@gmail.com>
2023-10-12 09:00:05 -0700

    Function as a parameter

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+/**
+ * int_index - fxn that returns the index of
+ * the first elements,for which the return
+ * of another fxn cmp is not zero
+ * @array: to search through
+ * @size: of the array
+ * @cmp: pointer to a fxn to be used to
+ * compare values
+ * Return: first int element found
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || size == NULL)
+		return (-1);
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index] != 0)
+				return (index);
+	}
+	return (-1);
+}
